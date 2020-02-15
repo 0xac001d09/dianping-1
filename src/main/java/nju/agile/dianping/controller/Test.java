c0496a59d15821d9db385160dda4aca659ffff09
taoze <896520209@qq.com>
2020-02-15 17:25:09 +0800

    前端

---
+++ src/main/java/nju/agile/dianping/controller/Test.java
@@ -0,0 +1,16 @@
+package nju.agile.dianping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller("/index")
+@RequestMapping("/index")
+public class Test {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "hello";
+    }
+}
