fd3bbbf04b99c7ad5f45d1170b5a280d1e95f74f
taoze <896520209@qq.com>
2020-02-15 17:22:09 +0800

    前端

--- src/main/java/nju/agile/dianping/DianpingApplication.java
+++ src/main/java/nju/agile/dianping/DianpingApplication.java
@@ -1,34 +1,13 @@
 package nju.agile.dianping;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-//@SpringBootApplication(scanBasePackages = {"nju.agile.dianping"})
-//@MapperScan("nju.agile.dianping.dal")
-//@EnableAspectJAutoProxy(proxyTargetClass = true)
-//public class DianpingApplication {
-//    public static void main(String[] args) {
-//        SpringApplication.run(DianpingApplication.class, args);
-//    }
-//}
-
-
-@SpringBootApplication(scanBasePackages = {"nju.agile.dianping"})
-@MapperScan("nju.agile.dianping.dal")
-@EnableAspectJAutoProxy(proxyTargetClass = true)
-public class DianpingApplication extends SpringBootServletInitializer {
+@SpringBootApplication
+public class DianpingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DianpingApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(DianpingApplication.class);
     }
 
 }
