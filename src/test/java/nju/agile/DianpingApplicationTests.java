fdf0875b94c4aff11e3d62ed5f999037f2e3df20
brynax0q <xq0x0q@163.com>
2020-02-15 15:06:48 +0800

    test

--- src/test/java/nju/agile/DianpingApplicationTests.java
+++ src/test/java/nju/agile/DianpingApplicationTests.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DianpingApplication {
-
+class DianpingApplicationTests {
     @Test
     void contextLoads() {
-    }
 
+    }
 }
